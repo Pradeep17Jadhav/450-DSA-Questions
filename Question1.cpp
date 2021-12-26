0c5009e854ecdb1641ed8135f55521342e92e50b
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2021-12-26 19:50:29 +0530

    Solved question 1

---
+++ Question1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int arr[], int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void reverse(int arr[], int n)
+{
+    for(int i = 0; i < n /2; i++)
+    {
+        int temp = arr[i];
+        arr[i] = arr[n - i - 1];
+        arr[n - i - 1] = temp;
+    }
+}
+
+
+int main(){
+    int n;
+    cout << "Enter a number of elements ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter elements: ";
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "Before: ";
+    printArray(arr, n);
+
+    reverse(arr, n);
+
+    cout << "After: ";
+    printArray(arr, n);
+
+    return 0;
+}
