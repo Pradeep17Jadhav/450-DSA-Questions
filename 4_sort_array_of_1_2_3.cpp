098ebc02d67119fa7835e2b1de977f20129f1802
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2021-12-27 23:10:24 +0530

    solved DNF sort algorithm

---
+++ 4_sort_array_of_1_2_3.cpp
@@ -0,0 +1,56 @@
+// Sort an array of 0s, 1s and 2s in ascending order.
+// Dutch National Flag Algorithm (DNF Sort Algorithm)
+// https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1
+// https://www.youtube.com/watch?v=2C4CQ32961Y
+
+#include <iostream>
+using namespace std;    
+    
+void sort012(int a[], int n)
+{
+    int low, mid, high, temp;
+    low = mid = 0;
+    high = n-1;
+    while(mid <= high)
+    {
+        if(a[mid] == 0)
+        {
+            temp = a[low];
+            a[low] = a[mid];
+            a[mid] = temp;
+            low++;
+            mid++;
+        }
+        else if(a[mid] == 1)
+        {
+            mid++;
+        }
+        else
+        {
+            temp = a[mid];
+            a[mid] = a[high];
+            a[high] = temp;
+            high--;
+        }
+    }
+}
+
+int main()
+{
+    int arr[10] = {2, 0, 1, 2, 0, 0, 2, 1, 1, 0};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    for(int i:arr)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    sort012(arr, n);
+    
+    for(int i:arr)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+}
