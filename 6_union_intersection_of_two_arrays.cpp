f351cc62c8397460ddee0523d6baa9fedbb9584d
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2021-12-28 18:43:08 +0530

    add extra solution to intersection code

--- 6_union_intersection_of_two_arrays.cpp
+++ 6_union_intersection_of_two_arrays.cpp
@@ -4,6 +4,8 @@
 
 #include<iostream>
 #include<set>
+#include<unordered_set>
+#include<vector>
 using namespace std;
 
 int doUnion(int a[], int n, int b[], int m)  {
@@ -19,7 +21,8 @@ int doUnion(int a[], int n, int b[], int m)  {
     return s.size();
 }
 
-int doIntersection(int a[], int n, int b[], int m) {
+//using sets
+int doIntersection1(int a[], int n, int b[], int m) {
     set<int> s;
     set<int> res;
     for(int i = 0; i < n; i++)
@@ -36,15 +39,38 @@ int doIntersection(int a[], int n, int b[], int m) {
     return res.size();
 }
 
+//using unordered sets and vector
+int doIntersection2(int a[], int n, int b[], int m) {
+    unordered_set<int> s;
+    vector<int> res;
+    for(int i = 0; i < n; i++)
+    {
+        s.insert(a[i]);
+    }
+
+    for(int i = 0; i < m; i++)
+    {
+        if(s.count(b[i]))
+        {
+            res.push_back(b[i]);
+            s.erase(b[i]);
+        }
+    }
+
+    return res.size();
+}
+
 int main()
 {
     int a[] = {85, 25, 1, 32, 54, 6};
     int n = sizeof(a) / sizeof(a[0]);
-    int b[] = {25, 2, 84, 1, 62};
+    int b[] = {25, 2, 84, 1, 62, 6};
     int m = sizeof(b) / sizeof(b[0]);
     int unionSize = doUnion(a, n, b, m);
-    int intersectionSize = doIntersection(a, n, b, m);
+    int intersectionSize1 = doIntersection1(a, n, b, m);
+    int intersectionSize2 = doIntersection2(a, n, b, m);
     cout << "Size of union array is: " << unionSize << endl;
-    cout << "Size of intersection array is: " << intersectionSize;
+    cout << "Size of intersection array 1 is: " << intersectionSize1 << endl;
+    cout << "Size of intersection array 2 is: " << intersectionSize2 << endl;
     return 0;
 }
